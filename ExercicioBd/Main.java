26ed5c9ce57e86b8f0f749b039f9545da3d843b2
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2023-11-13 20:22:42 -0300

    .

--- ExercicioBd/Main.java
+++ ExercicioBd/Main.java
@@ -1,5 +1,77 @@
-package ExercicioBd;
+import java.util.Scanner;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Criar usuário");
+            System.out.println("2. Alterar usuário");
+            System.out.println("3. Excluir usuário");
+            System.out.println("4. Visualizar usuários");
+            System.out.println("5. Logar");
+            System.out.println("0. Sair");
+
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer do scanner
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome: ");
+                    String nomeCriar = scanner.nextLine();
+                    System.out.print("Email: ");
+                    String emailCriar = scanner.nextLine();
+                    System.out.print("Senha: ");
+                    String senhaCriar = scanner.nextLine();
+
+                    Usuário usuarioCriar = new Usuário(0, nomeCriar, emailCriar, senhaCriar);
+                    usuarioCriar.inserirUsuario();
+                    break;
+
+                case 2:
+                    System.out.print("ID do usuário a ser alterado: ");
+                    int idAlterar = scanner.nextInt();
+                    scanner.nextLine(); // Limpar o buffer do scanner
+
+                    System.out.print("Novo nome: ");
+                    String novoNome = scanner.nextLine();
+                    System.out.print("Novo email: ");
+                    String novoEmail = scanner.nextLine();
+                    System.out.print("Nova senha: ");
+                    String novaSenha = scanner.nextLine();
+
+                    Usuário usuarioAlterar = new Usuário(idAlterar, novoNome, novoEmail, novaSenha);
+                    usuarioAlterar.atualizarUsuario();
+                    break;
+
+                case 3:
+                    System.out.print("ID do usuário a ser excluído: ");
+                    int idExcluir = scanner.nextInt();
+                    scanner.nextLine(); // Limpar o buffer do scanner
+
+                    Usuário usuarioExcluir = new Usuário(idExcluir, null, null, null);
+                    usuarioExcluir.excluirUsuario();
+                    break;
+
+                case 4:
+                    Usuário.visualizarUsuarios();
+                    break;
+
+                case 5:
+                    // Implemente a lógica de login conforme necessário
+                    System.out.println("Funcionalidade de login não implementada neste exemplo.");
+                    break;
+
+                case 0:
+                    System.out.println("Saindo...");
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+    }
 }
--- ExercicioBd/Usuário.java
+++ ExercicioBd/Usuário.java
@@ -1,39 +1,106 @@
 package ExercicioBd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Usuário {
 
     public int id;
-    public String name;
+    public String nome;
     public String email;
-    public String password;
+    public String senha;
 
-    public Usuário(int id, String name, String email, String password) {
+    public Usuário(int id, String nome, String email, String senha) {
         this.id = id;
-        this.name = name;
+        this.nome = nome;
         this.email = email;
-        this.password = password;
+        this.senha = senha;
     }
 
+    public void inserirUsuario() {
+        try (Connection connManager = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/usuario",
+                "root",
+                "")) {
 
+            System.out.println("Inserindo os valores nas variáveis");
+            try (PreparedStatement sc = connManager.prepareStatement("INSERT INTO usuario.usuario VALUES (?, ?, ?, ?)")) {
+                sc.setLong(1, 0); // auto-incremento no banco de dados
+                sc.setString(2, this.nome);
+                sc.setString(3, this.email);
+                sc.setString(4, this.senha);
+                sc.executeUpdate();
 
+                System.out.println("Usuário inserido");
+            }
+        } catch (SQLException exception) {
+            System.out.println("Erro ao inserir usuário: " + exception.getMessage());
+        }
+    }
 
+    public void atualizarUsuario() {
+        try (Connection connManager = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/usuario",
+                "root",
+                "")) {
 
+            System.out.println("Alterar valores dos usuários");
+            try (PreparedStatement sc = connManager.prepareStatement("UPDATE usuario.usuario SET nome=?, email=?, senha=? WHERE id=?")) {
+                sc.setString(1, this.nome);
+                sc.setString(2, this.email);
+                sc.setString(3, this.senha);
+                sc.setInt(4, this.id);
+                sc.executeUpdate();
 
+                System.out.println("Usuário atualizado com sucesso!");
+            }
+        } catch (SQLException exception) {
+            System.out.println("Erro ao atualizar usuário: " + exception.getMessage());
+        }
+    }
 
+    public void excluirUsuario() {
+        try (Connection connManager = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/usuario",
+                "root",
+                "")) {
 
+            System.out.println("Excluindo usuário");
+            try (PreparedStatement sc = connManager.prepareStatement("DELETE FROM usuario.usuario WHERE id=?")) {
+                sc.setInt(1, this.id);
+                sc.executeUpdate();
 
+                System.out.println("Usuário excluído com sucesso!");
+            }
+        } catch (SQLException exception) {
+            System.out.println("Erro ao excluir usuário: " + exception.getMessage());
+        }
+    }
 
+    public static void visualizarUsuarios() {
+        try (Connection connManager = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/usuario",
+                "root",
+                "")) {
 
+            System.out.println("Visualizando usuários");
+            try (PreparedStatement sc = connManager.prepareStatement("SELECT * FROM usuario.usuario");
+                 ResultSet rs = sc.executeQuery()) {
 
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nome = rs.getString("nome");
+                    String email = rs.getString("email");
+                    String senha = rs.getString("senha");
 
-
-
-
-
-
-
-
-
-
-  }
+                    System.out.println("ID: " + id + ", Nome: " + nome + ", Email: " + email + ", Senha: " + senha);
+                }
+            }
+        } catch (SQLException exception) {
+            System.out.println("Erro ao visualizar usuários: " + exception.getMessage());
+        }
+    }
+}
